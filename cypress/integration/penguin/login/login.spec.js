8af825323f1cb3ae9f14cd8111d1a32e025c88ab
pramam <57686495+pramam@users.noreply.github.com>
2022-04-22 20:26:57 -0700

    6 Login/Logout tests

--- cypress/integration/penguin/login/login.spec.js
+++ cypress/integration/penguin/login/login.spec.js
@@ -8,9 +8,52 @@ describe("UserStory: Login", () => {
     // console.log(`loginData` + loginData.username, loginData.password);
   });
 
-  it.only(`should be able to login with valid credentials`, function () {
+  it(`should be able to login with valid credentials`, function () {
     cy.login(loginData.username, loginData.password).ensureOnLoggedInPage(
       loginData.appID
     );
   });
+
+  it(`clearing the cookies should log you out`, function () {
+    cy.login(loginData.username, loginData.password).ensureOnLoggedInPage(
+      loginData.appID
+    );
+
+    cy.clearCookies();
+    cy.clearLocalStorage();
+    // At this point LocalStorage and SessionStorage have
+    // baseUrl on the left pane, but no key-value pairs stored as cookies
+
+    // Now you should be logged out
+    cy.ensureUserIsLoggedOut();
+  });
+
+  it(`should be able to logout from the UI`, function () {
+    cy.login(loginData.username, loginData.password).ensureOnLoggedInPage(
+      loginData.appID
+    );
+
+    cy.logout();
+    cy.ensureUserIsLoggedOutFromUI();
+  });
+
+  it(`should not be able to click on login when username and password are missing`, function () {
+    cy.visit("/");
+
+    cy.get('[data-cy="submit__btn"]').should("be.disabled");
+  });
+
+  it(`should not be able to click on login button with username present, password missing`, function () {
+    cy.cannotLoginWithMissingUsernameOrPassword(loginData.username, "");
+  });
+
+  it(`should not be able to click on login button with username missing, password present`, function () {
+    cy.cannotLoginWithMissingUsernameOrPassword(loginData.username, "");
+  });
+
+  // New tests to write:
+  // Extra long username, find limit of characters
+  // Extra long password, find limit of characters
+  // Add special characters in username
+  // Add special characters in password
 });
--- cypress/support/penguin/logincommands.js
+++ cypress/support/penguin/logincommands.js
@@ -6,6 +6,16 @@ Cypress.Commands.add("login", (username, password) => {
   cy.get('[data-cy="submit__btn"]').click();
 });
 
+Cypress.Commands.add(
+  "cannotLoginWithMissingUsernameOrPassword",
+  (username, password) => {
+    cy.visit("/");
+
+    if (username !== "") cy.get('[data-cy="username__input"]').type(username);
+    if (password !== "") cy.get('[data-cy="password__input"]').type(password);
+    cy.get('[data-cy="submit__btn"]').should("be.disabled");
+  }
+);
 Cypress.Commands.add("ensureOnLoggedInPage", (appID) => {
   cy.url().should("eq", Cypress.config().baseUrl + "/welcome");
 
@@ -14,5 +24,24 @@ Cypress.Commands.add("ensureOnLoggedInPage", (appID) => {
     "Complete Your Profile"
   );
 
-  cy.get(`a[href="/search/${appID}/"]`).should("exist");
+  cy.get(`a[href="/search/${appID}/"]`).should("be.visible");
+});
+
+Cypress.Commands.add("ensureUserIsLoggedOut", () => {
+  cy.get('[ng-if="header"]').should("have.text", "You've Been Logged Out");
+});
+
+Cypress.Commands.add("logout", () => {
+  // Click on Avatar circle
+  cy.get('span[class="acronym"]').click();
+  // Click on logout
+  cy.get('a[href="logout"]').click();
+});
+
+Cypress.Commands.add("ensureUserIsLoggedOutFromUI", () => {
+  cy.url().should("eq", Cypress.config().baseUrl + "/logout");
+  cy.get('[data-cy="logout__msg"]').should(
+    "have.text",
+    "You have successfully logged out"
+  );
 });
