53f34e3f82f9c49e136681f10fcb12771ae63729
pramam <57686495+pramam@users.noreply.github.com>
2022-04-23 16:56:07 -0700

    API Test: POST: should get an error on POST request with missing mandatory fields

---
+++ cypress/integration/penguin/apitests/postrequest.spec.js
@@ -0,0 +1,45 @@
+/// <reference types="cypress" />
+
+import { HTTP_CODES } from "../../../utils/penguin/httpstatuscodes";
+
+describe("UserStory: POST API", () => {
+  it(`should get an error on POST request with missing mandatory fields`, function () {
+    cy.fixture("penguin/logindata.json").as("loginData");
+    cy.fixture("penguin/appdata.json").as("appData");
+
+    let defaultRecordValueType =
+      "System.Collections.Generic.Dictionary`2[[System.String, mscorlib],[System.Object, mscorlib]], mscorlib";
+
+    cy.get("@loginData").then((loginData) => {
+      cy.get("@appData").then((appData) => {
+        cy.request({
+          method: "POST",
+          url: `${appData.APIUrl}/app/${appData.appID}/record`,
+          failOnStatusCode: false,
+          headers: {
+            accept: "application/json",
+            Authorization: "Bearer " + loginData.bearerToken,
+            "Private-Token": loginData.privateToken,
+          },
+          body: {
+            applicationId: `${appData.appID}`,
+            values: {
+              $type: defaultRecordValueType,
+              // "System.Collections.Generic.Dictionary`2[[System.String, mscorlib],[System.Object, mscorlib]], mscorlib",
+              RecordName: "Record Value",
+            },
+          },
+        }).then((res) => {
+          console.log(res.status);
+          console.log(JSON.stringify(res));
+          expect(res.status).to.eq(HTTP_CODES.BAD_REQUEST);
+          expect(res.body).has.property("ErrorCode", 5008);
+          expect(res.body).has.property(
+            "Argument",
+            "Field City must have value.\r\nField First Name must have value.\r\nField Last Name must have value."
+          );
+        });
+      });
+    });
+  });
+});
