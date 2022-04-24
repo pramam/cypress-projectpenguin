1d3e1b8a6f7a5e1f68bbfa3ea1f1ab233f333251
pramam <57686495+pramam@users.noreply.github.com>
2022-04-24 16:53:16 -0700

    API Tests: Cypress command POSTvalidatefull, validate \*simple\* non-mandatory fields

--- cypress/support/penguin/apivalidatecommands.js
+++ cypress/support/penguin/apivalidatecommands.js
@@ -4,6 +4,7 @@ import { RECORD_KEYS } from "../../utils/penguin/recordfields";
 
 Cypress.Commands.add("POSTvalidatefull", (response, sent) => {
   let postresbodyvalues = response.body.values;
+  // Validate mandatory values first
   expect(postresbodyvalues).has.property(
     RECORD_KEYS.RK_FIRST_NAME,
     sent.values.aHdR_gHQmRT8ItVTL
@@ -15,6 +16,31 @@ Cypress.Commands.add("POSTvalidatefull", (response, sent) => {
   expect(postresbodyvalues).has.property(
     RECORD_KEYS.RK_CITY,
     sent.values.aFjm80LnbJf780V6p
+  );
+  // Now validate the other fields in response.body.values
+  expect(postresbodyvalues).has.property(
+    RECORD_KEYS.RK_EMAIL,
+    sent.values.aGgc3qp6gt3dDR_na
+  );
+  expect(postresbodyvalues).has.property(
+    RECORD_KEYS.RK_STATE,
+    sent.values.aIaHwVkkr_seOK096
+  );
+  expect(postresbodyvalues).has.property(
+    RECORD_KEYS.RK_STREET_ADDRESS,
+    sent.values.aJDBDjjIFiTemxLGc
+  );
+  expect(postresbodyvalues).has.property(
+    RECORD_KEYS.RK_TELEPHONE,
+    sent.values.aJX7sLD3xZH9TlVps
+  );
+  expect(postresbodyvalues).has.property(
+    RECORD_KEYS.RK_TEXT_BOX,
+    sent.values.aJr4VxhqeQ4fAZgO7
+  );
+  expect(postresbodyvalues).has.property(
+    RECORD_KEYS.RK_ZIP,
+    sent.values.aKTyoAgO27gfZC0Vd
   );
   // without this return the .then chained on POSTvalidate is unable to get the original response
   return response;
