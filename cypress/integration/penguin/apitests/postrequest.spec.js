3112e2ac71f3f3c42678fcde30b18689f0ad825c
pramam <57686495+pramam@users.noreply.github.com>
2022-04-24 12:46:11 -0700

    API Tests: POST: "should be able to submit POST request without , with mandatory fields/confirm with GET"

--- cypress/integration/penguin/apitests/postrequest.spec.js
+++ cypress/integration/penguin/apitests/postrequest.spec.js
@@ -150,4 +150,72 @@ describe("UserStory: POST API", () => {
       });
     });
   });
+
+  it(`should be able to submit POST request without $type, with mandatory fields/confirm with GET`, function () {
+    cy.fixture("penguin/logindata.json").as("loginData");
+    cy.fixture("penguin/appdata.json").as("appData");
+    cy.fixture("penguin/testdata/mandatoryfieldswithout$type.json").as(
+      "postBody"
+    );
+
+    cy.get("@loginData").then((loginData) => {
+      cy.get("@appData").then((appData) => {
+        cy.get("@postBody").then((postBody) => {
+          cy.POSTrecord(appData, loginData, postBody)
+            .then((postres) => {
+              expect(postres.status).to.eq(HTTP_CODES.OK);
+
+              // Check that the post request has the right values
+              let postresbodyvalues = postres.body.values;
+              expect(postresbodyvalues).has.property(
+                RECORD_KEYS.RK_FIRST_NAME,
+                postBody.values.aHdR_gHQmRT8ItVTL
+              );
+              expect(postresbodyvalues).has.property(
+                RECORD_KEYS.RK_LAST_NAME,
+                postBody.values.aHxOeHmCTIGd_hg1b
+              );
+              expect(postresbodyvalues).has.property(
+                RECORD_KEYS.RK_CITY,
+                postBody.values.aFjm80LnbJf780V6p
+              );
+            })
+            .then((postres) => {
+              let resbody = postres.body;
+              // console.log(resbody);
+              let newrecordID = postres.body.id;
+              console.log(`NEW RECORD ID: ${newrecordID}`);
+
+              cy.GETrecordbyid(appData, loginData, newrecordID).then(
+                (getres) => {
+                  console.log(getres.status);
+                  expect(getres.status).to.eq(HTTP_CODES.OK);
+
+                  let getresbodyvalues = getres.body.values;
+                  // console.log(postBody.values.aHdR_gHQmRT8ItVTL);
+                  expect(getresbodyvalues).has.property(
+                    RECORD_KEYS.RK_FIRST_NAME,
+                    postBody.values.aHdR_gHQmRT8ItVTL
+                  );
+                  expect(getresbodyvalues).has.property(
+                    RECORD_KEYS.RK_LAST_NAME,
+                    postBody.values.aHxOeHmCTIGd_hg1b
+                  );
+                  expect(getresbodyvalues).has.property(
+                    RECORD_KEYS.RK_CITY,
+                    postBody.values.aFjm80LnbJf780V6p
+                  );
+                  // TODO: Clean this up
+                  // Hack for now
+                  // cy.writeFile(
+                  //   `temp/${newrecordID}-GET-output.json`,
+                  //   getres.body
+                  // );
+                }
+              );
+            });
+        });
+      });
+    });
+  });
 });
