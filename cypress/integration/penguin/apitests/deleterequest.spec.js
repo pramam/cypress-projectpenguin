b8e9d715f14a4e36520c02cb248a98a16375482d
pramam <57686495+pramam@users.noreply.github.com>
2022-04-24 18:17:40 -0700

    API Tests: DELETE: Code cleanup and added comments

--- cypress/integration/penguin/apitests/deleterequest.spec.js
+++ cypress/integration/penguin/apitests/deleterequest.spec.js
@@ -39,6 +39,14 @@ describe("UserStory: DELETE API", () => {
   });
 
   it(`should be able to delete record by id (mandatory fields)`, function () {
+    // We are going to do the following:
+    // - POST /app/{appId}/record  <a record with mandatory fields only>
+    // - Validate response of POST with `expect`
+    // - Get the record id of the new record from the POST response
+    // - Validate the response of GET with `expect`
+    // - Delete this new record using its id: DELETE /app/{appId}/record/{id}
+    // - Now do a GET /app/{appId}/record/{id} and we should get a 400 BAD REQUEST response as the
+    //   record has already been deleted
     cy.fixture("penguin/logindata.json").as("loginData");
     cy.fixture("penguin/appdata.json").as("appData");
     cy.fixture("penguin/testdata/mandatoryfields.json").as("postBody");
@@ -114,7 +122,7 @@ describe("UserStory: DELETE API", () => {
     });
   });
 
-  it.only(`should be able to delete record by id (all UI fields)`, function () {
+  it(`should be able to delete record by id (all UI fields)`, function () {
     // We are going to do the following:
     // - POST /app/{appId}/record  <a record with all UI fields> (meaning all fields that one can create using the UI)
     // - Validate response of POST with `POSTvalidatefull`
@@ -136,21 +144,6 @@ describe("UserStory: DELETE API", () => {
             cy.POSTvalidatefull(postres, postBody).then((validateres) => {
               console.log(`POSTvalidatebody succeeded`);
 
-              // // Check that the post request has the right values
-              // let postresbodyvalues = postres.body.values;
-              // // console.log(postBody.values.aHdR_gHQmRT8ItVTL);
-              // expect(postresbodyvalues).has.property(
-              //   RECORD_KEYS.RK_FIRST_NAME,
-              //   postBody.values.aHdR_gHQmRT8ItVTL
-              // );
-              // expect(postresbodyvalues).has.property(
-              //   RECORD_KEYS.RK_LAST_NAME,
-              //   postBody.values.aHxOeHmCTIGd_hg1b
-              // );
-              // expect(postresbodyvalues).has.property(
-              //   RECORD_KEYS.RK_CITY,
-              //   postBody.values.aFjm80LnbJf780V6p
-              // );
               let resbody = validateres.body;
               console.log(resbody);
               let newrecordID = postres.body.id;
@@ -190,51 +183,6 @@ describe("UserStory: DELETE API", () => {
               );
             });
           });
-
-          /*.then((postres) => {
-              let resbody = postres.body;
-              console.log(resbody);
-              let newrecordID = postres.body.id;
-              console.log(`NEW RECORD ID: ${newrecordID}`);
-
-              cy.GETrecordbyid(appData, loginData, newrecordID).then(
-                (getres) => {
-                  console.log(getres.status);
-                  expect(getres.status).to.eq(HTTP_CODES.OK);
-
-                  let getresbodyvalues = getres.body.values;
-                  console.log(postBody.values.aHdR_gHQmRT8ItVTL);
-                  expect(getresbodyvalues).has.property(
-                    RECORD_KEYS.RK_FIRST_NAME,
-                    postBody.values.aHdR_gHQmRT8ItVTL
-                  );
-                  expect(getresbodyvalues).has.property(
-                    RECORD_KEYS.RK_LAST_NAME,
-                    postBody.values.aHxOeHmCTIGd_hg1b
-                  );
-                  expect(getresbodyvalues).has.property(
-                    RECORD_KEYS.RK_CITY,
-                    postBody.values.aFjm80LnbJf780V6p
-                  );
-                  cy.DELETErecordbyid(appData, loginData, newrecordID).then(
-                    (deleteres) => {
-                      console.log(`DELETE status: ${deleteres.status}`);
-                      expect(deleteres.status).to.eq(HTTP_CODES.NO_CONTENT);
-
-                      // Now GET the same record and it should not be found
-                      cy.GETrecordbyid(appData, loginData, newrecordID).then(
-                        (getafterdeleteres) => {
-                          expect(getafterdeleteres.status).to.eq(
-                            HTTP_CODES.BAD_REQUEST
-                          );
-                        }
-                      );
-                    }
-                  );
-                }
-              );
-            });
-            */
         });
       });
     });
