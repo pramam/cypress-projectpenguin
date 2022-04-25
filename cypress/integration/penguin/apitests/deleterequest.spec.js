e8eca6ecc7d0653cfbeb130f05b1b5184490e998
pramam <57686495+pramam@users.noreply.github.com>
2022-04-24 17:54:29 -0700

    API Tests: DELETE: "should be able to delete record by id (all UI fields)"

--- cypress/integration/penguin/apitests/deleterequest.spec.js
+++ cypress/integration/penguin/apitests/deleterequest.spec.js
@@ -113,4 +113,81 @@ describe("UserStory: DELETE API", () => {
       });
     });
   });
+
+  it.only(`should be able to delete record by id (all UI fields)`, function () {
+    cy.fixture("penguin/logindata.json").as("loginData");
+    cy.fixture("penguin/appdata.json").as("appData");
+    cy.fixture("penguin/testdata/alluifields.json").as("postBody");
+
+    cy.get("@loginData").then((loginData) => {
+      cy.get("@appData").then((appData) => {
+        cy.get("@postBody").then((postBody) => {
+          cy.POSTrecord(appData, loginData, postBody)
+            .then((postres) => {
+              expect(postres.status).to.eq(HTTP_CODES.OK);
+
+              // Check that the post request has the right values
+              let postresbodyvalues = postres.body.values;
+              // console.log(postBody.values.aHdR_gHQmRT8ItVTL);
+              expect(postresbodyvalues).has.property(
+                RECORD_KEYS.RK_FIRST_NAME,
+                postBody.values.aHdR_gHQmRT8ItVTL
+              );
+              expect(postresbodyvalues).has.property(
+                RECORD_KEYS.RK_LAST_NAME,
+                postBody.values.aHxOeHmCTIGd_hg1b
+              );
+              expect(postresbodyvalues).has.property(
+                RECORD_KEYS.RK_CITY,
+                postBody.values.aFjm80LnbJf780V6p
+              );
+            })
+            .then((postres) => {
+              let resbody = postres.body;
+              console.log(resbody);
+              let newrecordID = postres.body.id;
+              console.log(`NEW RECORD ID: ${newrecordID}`);
+
+              cy.GETrecordbyid(appData, loginData, newrecordID).then(
+                (getres) => {
+                  console.log(getres.status);
+                  expect(getres.status).to.eq(HTTP_CODES.OK);
+
+                  let getresbodyvalues = getres.body.values;
+                  console.log(postBody.values.aHdR_gHQmRT8ItVTL);
+                  expect(getresbodyvalues).has.property(
+                    RECORD_KEYS.RK_FIRST_NAME,
+                    postBody.values.aHdR_gHQmRT8ItVTL
+                  );
+                  expect(getresbodyvalues).has.property(
+                    RECORD_KEYS.RK_LAST_NAME,
+                    postBody.values.aHxOeHmCTIGd_hg1b
+                  );
+                  expect(getresbodyvalues).has.property(
+                    RECORD_KEYS.RK_CITY,
+                    postBody.values.aFjm80LnbJf780V6p
+                  );
+                  cy.DELETErecordbyid(appData, loginData, newrecordID).then(
+                    (deleteres) => {
+                      console.log(`DELETE status: ${deleteres.status}`);
+                      expect(deleteres.status).to.eq(HTTP_CODES.NO_CONTENT);
+
+                      // Now GET the same record and it should not be found
+                      cy.GETrecordbyid(appData, loginData, newrecordID).then(
+                        (getafterdeleteres) => {
+                          expect(getafterdeleteres.status).to.eq(
+                            HTTP_CODES.BAD_REQUEST
+                          );
+                        }
+                      );
+                    }
+                  );
+                }
+              );
+            });
+        });
+      });
+    });
+  });
+
 });
